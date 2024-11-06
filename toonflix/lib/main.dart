84c926ecd7f6a2d826584327e1c1bb42e08345e7
siiion <siwonjeon234@gmail.com>
2024-11-06 23:27:21 +0900

    6주차: Pomodoro 앱 제작

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,87 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:toonflix/widgets/button.dart';
-import 'package:toonflix/widgets/currency_card.dart';
+import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
   runApp(const App());
 }
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
-
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  bool showTitle = true;
-
-  void toggleTitle() {
-    setState(() {
-      showTitle = !showTitle;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFFE7626C),
         textTheme: const TextTheme(
-          titleLarge: TextStyle(color: Colors.red),
+          displayLarge: TextStyle(
+            color: Color(0xFF232B55),
+          ),
         ),
+        cardColor: const Color(0xFFF4EDDB),
       ),
-      home: Scaffold(
-          backgroundColor: const Color(0xfff4eddb),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                showTitle ? const MyLargeTitle() : const Text('nothing'),
-                IconButton(
-                  onPressed: toggleTitle,
-                  icon: const Icon(Icons.remove_red_eye),
-                )
-              ],
-            ),
-          )),
-    );
-  }
-}
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  int count = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    print('initState!'); // 위젯 생성 시 출력
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    print("dispose!"); // 위젯 제거 시 출력
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print('build!');
-    return Text(
-      'My Large Title',
-      style: TextStyle(
-        fontSize: 30,
-        color: Theme.of(context).textTheme.titleLarge?.color,
-      ),
+      home: const HomeScreen(),
     );
   }
 }
